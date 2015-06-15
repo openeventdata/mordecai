db8697e2ce1bfaed015da0ea9e22ddf341844446
John Beieler <john.b30@gmail.com>
2015-06-15 16:46:23 -0400

    Vectorize comparison operations between word vectors.

--- country.py
+++ country.py
@@ -4,21 +4,20 @@
 #
 # osc.py is a full-pipeline version optimized for our OSC stories
 #
-# Example: curl -XPOST -H "Content-Type: application/json"  --data '{"text":"On 12 August, the BBC cited medical and security sources saying that fierce clashes broke out today in Tikrit, between the popular mobilization forces and elements of the terrorist DAISH. The sources added that the clashes resulted in the killing of 10 members of the popular mobilization and dozens from DAISH."}' 'http://192.168.50.236:8999/services/mordecai/osc' 
+# Example: curl -XPOST -H "Content-Type: application/json"  --data '{"text":"On 12 August, the BBC cited medical and security sources saying that fierce clashes broke out today in Tikrit, between the popular mobilization forces and elements of the terrorist DAISH. The sources added that the clashes resulted in the killing of 10 members of the popular mobilization and dozens from DAISH."}' 'http://192.168.50.236:8999/services/mordecai/osc'
 
 from __future__ import unicode_literals
-import json
-import requests
 import re
-import tangelo
-import sys, os
-import utilities
+import os
+import sys
 import glob
-from gensim import corpora, models, similarities, utils
+import json
+import tangelo
+import utilities
+from gensim import matutils
 from gensim.models import Word2Vec
 from unidecode import unidecode
 
-import pandas as pd
 from pyelasticsearch import ElasticSearch
 es = ElasticSearch(urls='http://localhost:9200', timeout=60, max_retries=2)
 
@@ -32,7 +31,7 @@ parser.read(config_file)
 mitie_directory = parser.get('Locations', 'mitie_directory')
 word2vec_model = parser.get('Locations', 'word2vec_model')
 
-sys.path.append(mitie_directory) 
+sys.path.append(mitie_directory)
 from mitie import *
 
 stopword_country_names = {"Afghanistan":"AFG", "Åland Islands":"ALA", "Albania":"ALB", "Algeria":"DZA",
@@ -42,74 +41,88 @@ stopword_country_names = {"Afghanistan":"AFG", "Åland Islands":"ALA", "Albania"
     "Austria":"AUT", "Azerbaijan":"AZE", "Bahamas":"BHS", "Bahrain":"BHR",
     "Bangladesh":"BGD", "Barbados":"BRB", "Belarus":"BLR", "Belgium":"BEL",
     "Belize":"BLZ", "Benin":"BEN", "Bermuda":"BMU", "Bhutan":"BTN",
-    "Bolivia":"BOL", "Bosnia_Herzegovina":"BIH", 
+    "Bolivia":"BOL", "Bosnia_Herzegovina":"BIH",
     "Botswana":"BWA", "Bouvet Island":"BVT", "Brazil":"BRA",
-    "Britain":"GBR", "Great_Britain":"GBR", 
-    "British Virgin Islands":"VGB", "Brunei":"BRN", "Bulgaria":"BGR", "Burkina_Faso":"BFA", 
+    "Britain":"GBR", "Great_Britain":"GBR",
+    "British Virgin Islands":"VGB", "Brunei":"BRN", "Bulgaria":"BGR", "Burkina_Faso":"BFA",
     "Burundi":"BDI", "Cambodia":"KHM", "Cameroon":"CMR",
-    "Canada":"CAN","Cape Verde":"CPV", "Cayman_Islands":"CYM", 
+    "Canada":"CAN","Cape Verde":"CPV", "Cayman_Islands":"CYM",
     "Central African Republic":"CAF", "Chad":"TCD", "Chile":"CHL", "China":"CHN",
     "Cocos_Islands":"CCK", "Colombia":"COL",
     "Comoros":"COM", "Congo Brazzaville":"COG", "Congo Kinshasa":"COD",
     "Congo":"COG", "Cook_Islands":"COK",
     "Costa_Rica":"CRI", "Cote Ivoire":"CIV", "Ivory_Coast":"CIV","Croatia":"HRV", "Cuba":"CUB",
     "Curaçao":"CUW", "Cyprus":"CYP", "Czech_Republic":"CZE", "Denmark":"DNK",
-    "Djibouti":"DJI", "Dominica":"DMA", "Dominican_Republic":"DOM", 
+    "Djibouti":"DJI", "Dominica":"DMA", "Dominican_Republic":"DOM",
     "Ecuador":"ECU", "Egypt":"EGY", "El_Salvador":"SLV",
     "Equatorial_Guinea":"GNQ", "Eritrea":"ERI", "Estonia":"EST", "Ethiopia":"ETH",
     "Falkland_Islands":"FLK", "Faroe_Islands":"FRO",
-    "Fiji":"FJI", "Finland":"FIN", "France":"FRA", "French_Guiana":"GUF", 
+    "Fiji":"FJI", "Finland":"FIN", "France":"FRA", "French_Guiana":"GUF",
     "French_Polynesia":"PYF","Gabon":"GAB",
     "Gambia":"GMB", "Gaza":"PSE", "Georgia":"GEO", "Germany":"DEU", "Ghana":"GHA",
     "Gibraltar":"GIB", "Greece":"GRC", "Greenland":"GRL", "Grenada":"GRD",
     "Guadeloupe":"GLP", "Guam":"GUM", "Guatemala":"GTM", "Guernsey":"GGY",
-    "Guinea":"GIN", "Guinea_Bissau":"GNB", "Guyana":"GUY", "Haiti":"HTI","Honduras":"HND", 
-    "Hong_Kong":"HKG",  "Hungary":"HUN", "Iceland":"ISL", 
-    "India":"IND", "Indonesia":"IDN", "Iran":"IRN", "Iraq":"IRQ", "Ireland":"IRL", 
+    "Guinea":"GIN", "Guinea_Bissau":"GNB", "Guyana":"GUY", "Haiti":"HTI","Honduras":"HND",
+    "Hong_Kong":"HKG",  "Hungary":"HUN", "Iceland":"ISL",
+    "India":"IND", "Indonesia":"IDN", "Iran":"IRN", "Iraq":"IRQ", "Ireland":"IRL",
     "Israel":"ISR", "Italy":"ITA", "Jamaica":"JAM", "Japan":"JPN",
     "Jordan":"JOR", "Kazakhstan":"KAZ", "Kenya":"KEN",
     "Kiribati":"KIR", "Kuwait":"KWT", "Kyrgyzstan":"KGZ", "Laos":"LAO",
     "Latvia":"LVA", "Lebanon":"LBN", "Lesotho":"LSO", "Liberia":"LBR",
     "Libya":"LBY", "Liechtenstein":"LIE", "Lithuania":"LTU", "Luxembourg":"LUX",
     "Macau":"MAC", "Macedonia":"MKD", "Madagascar":"MDG", "Malawi":"MWI",
-    "Malaysia":"MYS", "Maldives":"MDV", "Mali":"MLI", "Malta":"MLT", "Marshall_Islands":"MHL", 
+    "Malaysia":"MYS", "Maldives":"MDV", "Mali":"MLI", "Malta":"MLT", "Marshall_Islands":"MHL",
     "Martinique":"MTQ", "Mauritania":"MRT", "Mauritius":"MUS",
     "Mayotte":"MYT", "Mexico":"MEX", "Micronesia":"FSM", "Moldova":"MDA",
     "Monaco":"MCO", "Mongolia":"MNG", "Montenegro":"MNE", "Montserrat":"MSR",
     "Morocco":"MAR", "Mozambique":"MOZ", "Myanmar":"MMR", "Burma":"MMR", "Namibia":"NAM",
-    "Nauru":"NRU", "Nepal":"NPL", "Netherlands":"NLD", "Netherlands Antilles":"ANT", 
+    "Nauru":"NRU", "Nepal":"NPL", "Netherlands":"NLD", "Netherlands Antilles":"ANT",
     "New Caledonia":"NCL", "New_Zealand":"NZL", "Nicaragua":"NIC",
-    "Niger":"NER", "Nigeria":"NGA", "Niue":"NIU", "North_Korea":"PRK", 
+    "Niger":"NER", "Nigeria":"NGA", "Niue":"NIU", "North_Korea":"PRK",
     "Northern Ireland":"IRL", "Northern Mariana Islands":"MNP",
     "Norway":"NOR", "Oman":"OMN", "Pakistan":"PAK",
-    "Palau":"PLW", "Palestinian_Territories":"PSE", "Palestine":"PSE","Panama":"PAN", "Papua New Guinea":"PNG", 
-    "Paraguay":"PRY", "Peru":"PER", "Philippines":"PHL", "Pitcairn_Islands":"PCN", 
+    "Palau":"PLW", "Palestinian_Territories":"PSE", "Palestine":"PSE","Panama":"PAN", "Papua New Guinea":"PNG",
+    "Paraguay":"PRY", "Peru":"PER", "Philippines":"PHL", "Pitcairn_Islands":"PCN",
     "Poland":"POL", "Portugal":"PRT", "Puerto_Rico":"PRI",
     "Qatar":"QAT", "Réunion":"REU", "Romania":"ROU", "Russia":"RUS",
-    "Rwanda":"RWA", "Saint Barthélemy":"BLM", "Saint Helena":"SHN", 
+    "Rwanda":"RWA", "Saint Barthélemy":"BLM", "Saint Helena":"SHN",
     "Saint Kitts Nevis":"KNA", "Saint Lucia":"LCA",
-    "Saint Pierre Miquelon":"SPM", "Saint Vincent Grenadines":"VCT", 
+    "Saint Pierre Miquelon":"SPM", "Saint Vincent Grenadines":"VCT",
     "Samoa":"WSM", "San_Marino":"SMR", "São Tomé Príncipe":"STP", "Saudi_Arabia":"SAU",
-    "Senegal":"SEN", "Serbia":"SRB", 
-    "Seychelles":"SYC", "Sierra_Leone":"SLE", "Singapore":"SGP", "Sint Maarten":"SXM", 
+    "Senegal":"SEN", "Serbia":"SRB",
+    "Seychelles":"SYC", "Sierra_Leone":"SLE", "Singapore":"SGP", "Sint Maarten":"SXM",
     "Slovakia":"SVK", "Slovenia":"SVN", "Solomon_Islands":"SLB",
-    "Somalia":"SOM", "South_Africa":"ZAF", 
-    "South_Korea":"KOR", "South Sudan":"SSD", "Spain":"ESP", "Sri_Lanka":"LKA", "Sudan":"SDN", 
+    "Somalia":"SOM", "South_Africa":"ZAF",
+    "South_Korea":"KOR", "South Sudan":"SSD", "Spain":"ESP", "Sri_Lanka":"LKA", "Sudan":"SDN",
     "Suriname":"SUR", "Svalbard Jan Mayen":"SJM",
     "Swaziland":"SWZ", "Sweden":"SWE", "Switzerland":"CHE", "Syria":"SYR",
     "Taiwan":"TWN", "Tajikistan":"TJK", "Tanzania":"TZA", "Thailand":"THA",
-    "Timor Leste":"TLS", "East_Timor":"TLS","Togo":"TGO", "Tokelau":"TKL", "Tonga":"TON", "Trinidad Tobago":"TTO", 
+    "Timor Leste":"TLS", "East_Timor":"TLS","Togo":"TGO", "Tokelau":"TKL", "Tonga":"TON", "Trinidad Tobago":"TTO",
     "Tunisia":"TUN", "Turkey":"TUR",
-    "Turkmenistan":"TKM", "Turks Caicos Islands":"TCA", "Tuvalu":"TUV", "U.S. Minor Outlying Islands":"UMI", 
+    "Turkmenistan":"TKM", "Turks Caicos Islands":"TCA", "Tuvalu":"TUV", "U.S. Minor Outlying Islands":"UMI",
     "Virgin_Islands":"VIR", "Uganda":"UGA",
     "Ukraine":"UKR", "United_Arab_Emirates":"ARE", "United_Kingdom":"GBR",
-    "UK":"GBR", "United_States":"USA", "USA":"USA", "America":"USA", 
-    "Uruguay":"URY", "Uzbekistan":"UZB", "Vanuatu":"VUT", "Vatican":"VAT", "Venezuela":"VEN", 
+    "UK":"GBR", "United_States":"USA", "USA":"USA", "America":"USA",
+    "Uruguay":"URY", "Uzbekistan":"UZB", "Vanuatu":"VUT", "Vatican":"VAT", "Venezuela":"VEN",
     "Vietnam":"VNM", "Wallis Futuna":"WLF",
     "Western_Sahara":"ESH", "Yemen":"YEM", "Zambia":"ZMB", "Zimbabwe":"ZWE"}
 
-prebuilt = Word2Vec.load_word2vec_format(word2vec_model, binary=True) 
+prebuilt = Word2Vec.load_word2vec_format(word2vec_model, binary=True)
 vocab_set = set(prebuilt.vocab.keys())
+
+countries = stopword_country_names.keys()
+
+idx_country_mapping = {}
+index = numpy.empty(shape=(len(countries), 300), dtype=dtype)
+
+for idx, country in enumerate(countries):
+    country = unidecode(country)
+    try:
+        vector = prebuilt[country]
+    except KeyError:
+        pass
+    index[idx] = vector
+    idx_country_mapping[idx] = stopword_country_names[country]
 
 
 @tangelo.restful
@@ -122,10 +135,11 @@ focus by comparing the word2vec vectors for the places mentioned in the text
 with the vector representation of each country in the world, picking the
 closest."""
 
+
 @tangelo.restful
 def post(*arg, **kwargs):
     params = json.loads(tangelo.request_body().read())
-    text  = params['text']
+    text = params['text']
     out = utilities.talk_to_mitie(text)
     places = []
     miscs = []
@@ -134,46 +148,34 @@ def post(*arg, **kwargs):
             places.append(i['text'])
         if i['tag'] == "MISC" or i['tag'] == "misc":
             miscs.append(i['text'])
-    
+
     loc_list = [re.sub(" ", "_", element) for element in places]
     locs = [x for x in loc_list if x in vocab_set]
-    output_list = []
-    
+
     if locs:
-        for i in stopword_country_names.keys():
-            country = unidecode(i)
-            country = country.split()
-            score = prebuilt.n_similarity(locs, country) #utils.tokenize(text)
-            out = [i, score]
-            output_list.append(out)
-        df = pd.DataFrame(output_list)
-        country_name = df.sort([1], ascending=False).head(1)
-        country_name = country_name.reset_index()[0].tolist()[0]
+        locs_word_vec = [prebuilt[word] for word in locs]
+        locs_vec = numpy.array(locs_word_vec)
+        weights = numpy.dot(index,
+                            matutils.unitvec(locs_vec.mean(axis=0)).T).T
+        ranks = weights.argsort()[::-1]
         try:
-            return stopword_country_names[country_name]
+            return idx_country_mapping[ranks[0]]
         except:
             return []
     else:
         misc_list = [re.sub(" ", "_", element) for element in miscs]
         misc = [x for x in misc_list if x in vocab_set]
-        print misc
-        output_list = []
 
-        for i in stopword_country_names.keys():
-            country = unidecode(i)
-            country = country.split()
-            score = prebuilt.n_similarity(misc, country) #utils.tokenize(text)
-            out = [i, score]
-            output_list.append(out)
-        df = pd.DataFrame(output_list)
-        country_name = df.sort([1], ascending=False).head(1)
-        country_name = country_name.reset_index()[0].tolist()[0]
+        misc_word_vec = [prebuilt[word] for word in misc]
+        misc_vec = numpy.array(misc_word_vec)
+        weights = numpy.dot(index,
+                            matutils.unitvec(misc_vec.mean(axis=0)).T).T
+        ranks = weights.argsort()[::-1]
         try:
-            return stopword_country_names[country_name]    
-        except: 
+            return idx_country_mapping[ranks[0]]
+        except:
             return []
-    #return json.dumps(bothn)
-    
+
 
 # future: add place for title here?
 #    bothn = []
@@ -183,8 +185,8 @@ def post(*arg, **kwargs):
 #        if t:
 #            print "Match!!!!"
 #            bothn.append(placenames[n])
-#       
-# 
+#
+#
 #    if bothn == []:
 #        print "Using text_to_country"
 #    #    print utilities.text_to_country(text)
