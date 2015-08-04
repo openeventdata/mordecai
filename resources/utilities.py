612040860f81827bd65fa995ec23b24eef1c4077
chilland <casey.hilland@gmail.com>
2015-08-04 10:47:37 -0400

    Add boosting

--- resources/utilities.py
+++ resources/utilities.py
@@ -108,7 +108,7 @@ def mitie_context(text, ner_model):
 
 def query_geonames(conn, placename, country_filter):
     country_lower = [x.lower() for x in country_filter]
-    q = MultiMatch(query=placename, fields=['asciiname', 'alternativenames'])
+    q = MultiMatch(query=placename, fields=['asciiname^5', 'alternativenames'])
     res = conn.filter('term', country_code3=country_lower).query(q).execute()
     out = {'hits': {'hits': []}}
     keys = [u'admin1_code', u'admin2_code', u'admin3_code', u'admin4_code',
@@ -128,7 +128,7 @@ def query_geonames(conn, placename, country_filter):
 def query_geonames_featureclass(conn, placename, country_filter, feature_class):
     country_lower = [x.lower() for x in [country_filter]]
     feature_lower = [x.lower() for x in feature_class]
-    q = MultiMatch(query=placename, fields=['asciiname', 'alternativenames'])
+    q = MultiMatch(query=placename, fields=['asciiname^5', 'alternativenames'])
     res = conn.filter('term', country_code3=country_lower).filter('term', feature_class=feature_lower).query(q).execute()
     out = {'hits': {'hits': []}}
     keys = [u'admin1_code', u'admin2_code', u'admin3_code', u'admin4_code',
