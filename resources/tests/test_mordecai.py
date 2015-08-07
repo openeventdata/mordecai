688294db682986e14fa79e8b8eb22b5f9a795793
chilland <casey.hilland@gmail.com>
2015-08-07 09:41:34 -0400

    Skip ES test

--- resources/tests/test_mordecai.py
+++ resources/tests/test_mordecai.py
@@ -1,17 +1,22 @@
+import os
 from ..country import CountryAPI
 from ..places import PlacesAPI
 
 
 def test_places_api_one():
-    a = PlacesAPI()
-    locs = {u'entities': [{u'context': ['meeting', 'happened', 'in', '.'],
-                           u'score': 1.3923831181343844, u'tag': u'LOCATION',
-                           u'text': 'Ontario'}]}
+    if os.environ['CI']:
+        a = PlacesAPI()
+        locs = {u'entities': [{u'context': ['meeting', 'happened', 'in', '.'],
+                            u'score': 1.3923831181343844, u'tag': u'LOCATION',
+                            u'text': 'Ontario'}]}
 
-    result = a.process(locs, 'CAN')
-    gold = [{u'countrycode': u'CAN', u'lat': 43.65004, u'lon': -79.90554,
-             u'placename': u'SunnyView Dental', u'searchterm': 'Ontario'}]
-    assert result == gold
+        result = a.process(locs, 'CAN')
+        gold = [{u'countrycode': u'CAN', u'lat': 43.65004, u'lon': -79.90554,
+                u'placename': u'SunnyView Dental', u'searchterm': 'Ontario'}]
+        assert result == gold
+    else:
+        pass
+
 
 def test_country_process_one():
     a = CountryAPI()
