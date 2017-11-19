529628656571b46c8ae11caf5f38548239cceb08
Andy <ahalterman0@gmail.com>
2017-11-19 16:03:29 -0500

    Finish packaging stuff. PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mordecai',
-      version='2.0.0a0',
+      version='2.0.0a1',
       description='Full text geoparsing and event geocoding',
       url='http://github.com/openeventdata/mordecai/',
       author='Andy Halterman',
@@ -16,4 +16,4 @@ setup(name='mordecai',
                              'stopword_country_names.json'],
                     'models' : ['country_model.h5',
                                 'rank_model.h5']}
-      )
+     )
