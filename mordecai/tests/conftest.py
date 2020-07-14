97edbee5813b8a87606b8fb3d09b4f116cdaf025
Andy <ahalterman0@gmail.com>
2020-07-14 15:50:14 -0400

    Remove hardcoded paths from testing

--- mordecai/tests/conftest.py
+++ mordecai/tests/conftest.py
@@ -6,8 +6,8 @@ nlp = spacy.load('en_core_web_lg', disable=['parser', 'tagger'])
 
 @pytest.fixture(scope='session', autouse=True)
 def geo():
-    return Geoparser(nlp=nlp, threads=False, models_path = "/Users/ahalterman/MIT/Geolocation/mordecai_new/mordecai/mordecai/models/")
+    return Geoparser(nlp=nlp, threads=False)
 
 @pytest.fixture(scope='session', autouse=True)
 def geo_thread():
-    return Geoparser(nlp=nlp, threads=True, models_path = "/Users/ahalterman/MIT/Geolocation/mordecai_new/mordecai/mordecai/models/")
+    return Geoparser(nlp=nlp, threads=True)
